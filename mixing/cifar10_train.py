3469f623945e5bc47b30e1673285c76fa4abea7e
Mayank Pandey <mayankp@Mayanks-MacBook-Pro.local>
2016-06-22 16:45:52 -0500

    modified dirs

--- mixing/cifar10_train.py
+++ mixing/cifar10_train.py
@@ -50,7 +50,7 @@ import cifar10
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('train_dir',
-	'~/tfRuns/outputs/normalized_relu/tmp/train',
+	'~/tfRuns/outputs/mixing/train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_epochs', 64*2+32,
