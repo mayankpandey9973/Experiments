0876debf8f47dd0290a94c0ddc178053d7ff41e8
mayankpandey9973 <mayankpandey9973@gmail.com>
2016-06-24 15:43:08 -0500

    added '.'

--- current/cifar10_train.py
+++ current/cifar10_train.py
@@ -135,7 +135,7 @@ def train():
         sec_per_batch = float(duration)
 
         format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                      'sec/batch), writing to ' + FLAGS.train_dir)
+                      'sec/batch), writing to ' + FLAGS.train_dir + '.')
         print (format_str % (datetime.now(), step, loss_value,
                              examples_per_sec, sec_per_batch))
 
